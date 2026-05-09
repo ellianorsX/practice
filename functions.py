c8a23d77052b3b7577865f6d8e26989c2ef5f722
ellianorsX <marjonathe98@icloud.com>
2026-05-09 15:54:32 +0900

    feat: Learn Scope in PYTHON

--- functions.py
+++ functions.py
@@ -48,3 +48,19 @@ print("result3:", result3)
 
 result4 = give_greet("Ellia")
 print("result4:", result4)
+
+
+
+print("===== Scope =====")
+
+b = 100  # 3
+
+
+# DEFINE
+def calculate(a):  # 2
+    c = a * b  # 1
+    print(f"the c value: {c}")
+
+
+# CALL
+calculate(5)
