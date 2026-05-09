35e0817203855ab3be0dc58e2cc03c1869b31105
ellianorsX <marjonathe98@icloud.com>
2026-05-09 15:18:53 +0900

    feat: Learn string in PYTHON

--- primative.py
+++ primative.py
@@ -7,6 +7,29 @@ count = 100
 count_type = type(count)
 print(f"the count: {count} and type: count_type")
 
-result = count.bit_count() #method 
-result = count.numerator #state
+result1 = count.bit_count() #method 
+result2 = count.numerator #state
 print(result1, result2)
+
+
+print("==========  string  ===========")
+# METHODS: upper(), lower(), title(), find(), replace()
+
+
+
+course = "AI Python FullStack"
+result = type(course)
+print(f"the type of course: {result}")
+
+
+result = course.title()
+print(f"the result: {result}")
+
+result = course.upper()
+print(f"the result: {result}")
+
+result = course.replace("FullStack", "MasterClass")
+print(f"the result (4): {result}")
+
+print("=========  boolean  ============")
+
