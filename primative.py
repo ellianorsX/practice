f4b84831a82a45c8a7c3f01e8f4dc2966af5a962
ellianorsX <marjonathe98@icloud.com>
2026-05-09 15:07:30 +0900

    feat: Learn numbers in PYTHON

---
+++ primative.py
@@ -0,0 +1,12 @@
+print("===============================")
+# in JAVA, variable is a name storage lacation!
+# in PYTHON, variable is named referance!
+
+
+count = 100 
+count_type = type(count)
+print(f"the count: {count} and type: count_type")
+
+result = count.bit_count() #method 
+result = count.numerator #state
+print(result1, result2)
