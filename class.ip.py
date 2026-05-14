6d336d1fbe88a438197b824a6c74826307e501b2
ellianorsX <marjonathe98@icloud.com>
2026-05-14 12:26:31 +0900

    feat: Learn POLYMORPHISM concept in PYTHON

--- class.ip.py
+++ class.ip.py
@@ -37,14 +37,34 @@ class Dog(Animal): #child
         
         
 dog = Dog("Rex", "wow", True)
-cat = Cat("Tom", "myeow", True)
-fish = Fish("Nemo", "ZzZ", False)        
+# cat = Cat("Tom", "myeow", True)
+# fish = Fish("Nemo", "ZzZ", False)        
             
             
 dog.introdece()
-cat.introduce()
-fish.introduce()
+# cat.introduce()
+# fish.introduce()
 
 print("========")
-dog,make_voice()
-fish.make_voice()            
+dog.make_voice()
+# fish.make_voice()            
+
+
+
+
+print("===== POLYMORPHISM =====")
+# dog > dog > Animal > object
+
+print("-----------")
+a = isinstance(dog, Dog)
+b = isinstance(dog, Animal)
+c = isinstance(dog, object)
+d = isinstance("MIT", object )
+
+result = a and b and c and d
+print(f"trhe result: {result}")
+
+# dog > animal >object
+data1 = isinstance(Dog, Animal)
+data2 = isinstance(Animal, object)
+print("data:", data1, data2)
