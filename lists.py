4a213e1b220cf37ee9e09906dc8627781beb371a
ellianorsX <marjonathe98@icloud.com>
2026-05-16 12:15:43 +0900

    feat: Learn  enumerate, map and filter in PYTHON

--- lists.py
+++ lists.py
@@ -124,3 +124,61 @@ print("people(1)", people)
 # sort by age via lambda
 people.sort(key=lambda person: person[1])
 print("people(2)", people)
+
+
+print("===== enumerate, map and filter =====")
+
+# enumerate for index & value
+
+animals = ["dog", "cat", "fish"]  # list
+
+for element in enumerate(animals):
+    print("element:", element)
+
+
+for (index, value) in enumerate(animals):
+    print(f"the index: {index} and value: {value}")
+
+
+print("______")
+
+# similar in dictionaries
+
+car_obj = dict(brand="Ferrari", year=2025)  # dict
+result = car_obj.items()
+
+for (key, value) in result:
+    print(f"the key: {key} and value: {value}")
+    
+    
+    
+print("======= 4-Bo'lim")
+
+# map
+
+cars = [
+    ("Ferrari", 78),
+    ("Toyota", 87),
+    ("Audi", 116),
+    ("BMW", 109),
+    ("Pagani", 33)
+]
+
+result_map = map(lambda car: car[0], cars)
+
+print(f"the result_map: {result_map} and type: {type(result_map)}")
+
+new_cars = list(result_map)
+
+print("new_cars", new_cars)
+
+
+print("______")
+
+# filter
+
+result_filter = filter(lambda car: car[1] > 80, cars)
+
+print(f"the result_filter: {result_filter} and type: {type(result_filter)}")
+
+print(list(result_filter))
